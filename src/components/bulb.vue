bf5ebe5aab925996a0a633029881ba849009a8fb
Heath Williams <awilli01@amgen.com>
2017-10-11 11:11:52 -0400

    Added lots of funct for our light using webpack

---
+++ src/components/bulb.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="bulb" v-bind:class="lightColor"></div>
+</template>
+
+<script>
+	export default {
+		name : 'bulb',
+		props : [ 'lightColor' ]
+	};
+</script>
+
+<style>
+	.bulb {
+		margin : 5px;
+		border : 2px solid #000;
+		width : 50px;
+		height : 50px;
+		border-radius : 50%;
+		background-color : #ccc;
+	}
+
+	.green {
+		background-color : #0f0;
+	}
+
+	.yellow {
+		background-color : #ff0;
+	}
+
+	.red {
+		background-color : #f00;
+	}
+</style>
